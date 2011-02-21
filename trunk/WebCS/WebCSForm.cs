88ca06d2f2dd086b755de83c7a4f9890d117f70f
23pointsNorth@gmail.com <23pointsNorth@gmail.com@de578a6d-f750-1e1b-ecde-39dc7694b71e>
2011-02-21 11:06:35 +0000

    reference path edited; fixed small bugs;

--- trunk/WebCS/WebCSForm.cs
+++ trunk/WebCS/WebCSForm.cs
@@ -383,6 +383,9 @@ namespace WebCS
 
                 firstMarkerChangeRadButton.Enabled = false;
                 secondMarkerChangeRadButton.Enabled = false;
+
+                firstMarkerChangeColor = false;
+                secondMarkerChangeColor = false;
             }
 
             CheckEnabledTracking();
