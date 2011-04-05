7f2e8bf3e11100b883f20743f469c53c1b82aa44
23pointsNorth@gmail.com <23pointsNorth@gmail.com@de578a6d-f750-1e1b-ecde-39dc7694b71e>
2011-04-05 20:00:06 +0000

    fixed small bug with fixed numbers;

--- WebCS/BitmapDraw.cs
+++ WebCS/BitmapDraw.cs
@@ -1,107 +1,109 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
 
-static class BitmapDraw
+namespace BitmapProcessing
 {
-    public static Bitmap FilledRectangle(int width, int height, Color fillColour)
+    static class BitmapDraw
     {
-        Bitmap filledBitmap = new Bitmap(width, height);
-
-        using (Graphics graphicsOnImage = Graphics.FromImage(filledBitmap))
-        using (SolidBrush brush = new SolidBrush(fillColour))
+        public static Bitmap FilledRectangle(int width, int height, Color fillColour)
         {
-            graphicsOnImage.FillRectangle(brush, 0, 0, width, height);
-        }
-        return filledBitmap;
-    }
+            Bitmap filledBitmap = new Bitmap(width, height);
 
-    public static void Rectangle(Bitmap image, Rectangle rect, Pen pen)
-    {
-        using (Graphics g = Graphics.FromImage(image))
-        {
-            g.DrawRectangle(pen, rect);
-        }
-    }
-
-    public static void Rectangle(Bitmap image, Rectangle[] rect, Pen pen)
-    {
-        using (Graphics g = Graphics.FromImage(image))
-        {
-            foreach (var r in rect)
+            using (Graphics graphicsOnImage = Graphics.FromImage(filledBitmap))
+            using (SolidBrush brush = new SolidBrush(fillColour))
             {
-                g.DrawRectangle(pen, r);
+                graphicsOnImage.FillRectangle(brush, 0, 0, width, height);
             }
+            return filledBitmap;
         }
-    }
 
-    public static void Rectangle(Bitmap image, Rectangle[] rect, Pen[] pen)
-    {
-        if (rect.Length == pen.Length)
+        public static void Rectangle(Bitmap image, Rectangle rect, Pen pen)
         {
             using (Graphics g = Graphics.FromImage(image))
             {
-                for (int i = 0; i < rect.Length; i++)
-                {
-                    g.DrawRectangle(pen[i], rect[i]);
-                }
+                g.DrawRectangle(pen, rect);
             }
         }
-    }
 
-
-    public static void Rectangle(Bitmap image, Rectangle[] rect, Color[] color)
-    {
-        if (rect.Length == color.Length)
+        public static void Rectangle(Bitmap image, Rectangle[] rect, Pen pen)
         {
             using (Graphics g = Graphics.FromImage(image))
             {
-                for (int i = 0; i < rect.Length; i++)
+                foreach (var r in rect)
                 {
-                    g.DrawRectangle(new Pen(color[i], 2), rect[i]);
+                    g.DrawRectangle(pen, r);
                 }
             }
         }
-    }
 
-    public static void Rectangle(Bitmap image, Dictionary<Rectangle,Color> rectangles)
-    {
-        using (Graphics g = Graphics.FromImage(image))
+        public static void Rectangle(Bitmap image, Rectangle[] rect, Pen[] pen)
         {
-            foreach (var rect in rectangles)
+            if (rect.Length == pen.Length)
             {
-                g.DrawRectangle(new Pen(rect.Value, 2), rect.Key);
+                using (Graphics g = Graphics.FromImage(image))
+                {
+                    for (int i = 0; i < rect.Length; i++)
+                    {
+                        g.DrawRectangle(pen[i], rect[i]);
+                    }
+                }
             }
         }
-    }
 
-    public static Bitmap Resize(Bitmap toResize, int newWidth, int newHeight)
-    {
-        Bitmap result = new Bitmap(newWidth, newHeight);
-        using (Graphics graphic = Graphics.FromImage((System.Drawing.Image)result))
-            graphic.DrawImage(toResize, 0, 0, newWidth, newHeight);
-        return result;
-    }
-
-    public static Bitmap WriteString(Bitmap layer,string text)
-    {
-        Bitmap emptyBitmap = (Bitmap)layer.Clone();
-        using (Graphics g = Graphics.FromImage(emptyBitmap))
+        public static void Rectangle(Bitmap image, Rectangle[] rect, Color[] color)
         {
-            StringFormat strFormat = new StringFormat();
-            strFormat.Alignment = StringAlignment.Center;
-            strFormat.LineAlignment = StringAlignment.Center;
-            g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
-            g.DrawString(text, new Font("Arial", 20), Brushes.Black,
-                new RectangleF(0, 0, 352, 288), strFormat);
+            if (rect.Length == color.Length)
+            {
+                using (Graphics g = Graphics.FromImage(image))
+                {
+                    for (int i = 0; i < rect.Length; i++)
+                    {
+                        g.DrawRectangle(new Pen(color[i], 2), rect[i]);
+                    }
+                }
+            }
         }
-        return emptyBitmap;
-    }
 
-    public static void WriteString(Bitmap layer, string text,Color drawColor, Point location)
-    {
-        using (Graphics g = Graphics.FromImage(layer))
+        public static void Rectangle(Bitmap image, Dictionary<Rectangle, Color> rectangles)
         {
-            g.DrawString(text, new Font("Arial", 10), new SolidBrush(drawColor), location);
+            using (Graphics g = Graphics.FromImage(image))
+            {
+                foreach (var rect in rectangles)
+                {
+                    g.DrawRectangle(new Pen(rect.Value, 2), rect.Key);
+                }
+            }
+        }
+
+        public static Bitmap Resize(Bitmap toResize, int newWidth, int newHeight)
+        {
+            Bitmap result = new Bitmap(newWidth, newHeight);
+            using (Graphics graphic = Graphics.FromImage((System.Drawing.Image)result))
+                graphic.DrawImage(toResize, 0, 0, newWidth, newHeight);
+            return result;
+        }
+
+        public static Bitmap WriteString(Bitmap layer, string text)
+        {
+            Bitmap emptyBitmap = (Bitmap)layer.Clone();
+            using (Graphics g = Graphics.FromImage(emptyBitmap))
+            {
+                StringFormat strFormat = new StringFormat();
+                strFormat.Alignment = StringAlignment.Center;
+                strFormat.LineAlignment = StringAlignment.Center;
+                g.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                g.DrawString(text, new Font("Arial", 20), Brushes.Black,
+                    new RectangleF(0, 0, Constants.IMAGE_WIDTH, Constants.IMAGE_HEIGHT), strFormat);
+            }
+            return emptyBitmap;
+        }
+
+        public static void WriteString(Bitmap layer, string text, Color drawColor, Point location)
+        {
+            using (Graphics g = Graphics.FromImage(layer))
+            {
+                g.DrawString(text, new Font("Arial", 10), new SolidBrush(drawColor), location);
+            }
         }
     }
 }
